0f66d92103397edc50d23fea7a3d989e4114aefd
AleJ2 <59931695+AJ55P@users.noreply.github.com>
2023-02-21 10:22:07 -0800

    Main.js component work and creating custom css rules
    * Remove the import of the slider carousel component because it's a future feature
    * Comment out the breakfast recipes since they are part of the carousel feature
    * Strip out the nested <main> and <section> tag to reduce unnecessary nesting and complexity
    * Comment out the actual carousel implementation
    * Create custom css rules for creating a responsive grid layout, box-shadow for cool popup effect, and an active tab background color
    * Apply a responsive grid layout to Main.js using the custom css grid layout rule I added to the stylesheet

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {Slider} from './Slider';
-
 function Main({homeRecipes, setRecipeId}){
-    let breakfast_recipes = homeRecipes.breakfast_recipes;
+    // These breakfast recipes would be for the carousel!
+    // let breakfast_recipes = homeRecipes.breakfast_recipes;
     let primal_recipes = homeRecipes.primal_recipes;
     let recipeItems = primal_recipes.map(function(r){
         return <div onClick={() => setRecipeId(r.id)} className="group hover:scale-105 transition-transform shadow-2xl cursor-pointer flex flex-col flex-wrap justify-around items-center first-letter:cursor-pointer" key={r.id}>
@@ -14,18 +13,14 @@ function Main({homeRecipes, setRecipeId}){
               </div>;
     });
     return (
-        <main className="">
-            <section className="max-w-5xl mx-auto">
-                <h2 className="text-center mt-4 mb-4">Popular Primal Recipes</h2>
-                <div className="grid grid-cols-3 gap-4">
-                    {recipeItems}
-                </div>
-            </section>
-            <div className="max-w-5xl mx-auto mt-32">
-                <h2 className="text-center mb-6">Random Breakfast!</h2>
-                <Slider breakfastItems={breakfast_recipes}/>    
+        <>
+            <h2 className="text-center mt-4 mb-4">Popular Primal Recipes</h2>
+            <div className="grid grid-cols-autofit gap-4">
+                {recipeItems}
             </div>
-        </main>
+            {/* Potential Carousel Here! */}
+            {/* <Carousel /> */}
+        </>
     );
 }
 export default Main;
