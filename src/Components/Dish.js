5f6d36a0b36048bd3073cd27c2d55b56c8170f9b
AleJ2 <59931695+AJ55P@users.noreply.github.com>
2023-02-22 19:31:16 -0800

    Dish.js work
    * Import the TabInfoBox component and pass the ingredients, directions, and nutrition facts as props to it
    
    * Reword the variable, ingredientsUsed, to ingredients
    * Create a new variable, nutritionData, to store an array of nutrient objects
    * Give the <li> elements for the ingredients list color for its markers, bigger line height, disc markers, and slight margin on the bottom
    * Give the <li> elements from the directions list, numbered markers as well as add bottom margin to create space in between each direction
    * Strip out the <div>'s making up a temp tabinfo box as I have implemented it in its own component
    * Create a simple dish view that contains the dish name and a responsive grid layout for the recipe image and tabinfo box

--- src/Components/Dish.js
+++ src/Components/Dish.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import TabInfoBox from './TabInfoBox';
 
 function Dish({currRecipe}){
-    let ingredientsUsed = [];
+    let ingredients = [];
     let directions = [];
+    let nutritionData = [];
 
     if(currRecipe != null){
-        ingredientsUsed = currRecipe.extendedIngredients.map(e => 
-            <li key={e.id}>
+        nutritionData = currRecipe.nutrition.nutrients;
+        ingredients = currRecipe.extendedIngredients.map(e => 
+            <li className="marker:text-sky-400 leading-9 list-disc mb-1" key={e.id}>
                 {e.original}
             </li>
         );
@@ -15,25 +18,22 @@ function Dish({currRecipe}){
         */
         if(currRecipe.analyzedInstructions.length > 0){
             directions = currRecipe.analyzedInstructions[0].steps.map(e => 
-                <li key={e.number}>
+                <li className="list-decimal mb-5" key={e.number}>
                     {e.step}
                 </li>
             );
         }
     }
     return (
-        <div>
-            {currRecipe && <h1 className="text-2xl text-center">{currRecipe.title}</h1>}
-            <div>
-                <h2>Ingredients</h2>
-                <ul className="list-disc">{ingredientsUsed}</ul>
+        <>
+            <header className="mt-7 mb-5">
+                {currRecipe && <h1 className=" text-2xl text-center">{currRecipe.title}</h1>}
+            </header>
+            <div className="grid grid-cols-autofit justify-center gap-x-10 gap-y-5">
+                {currRecipe && <img className="justify-self-end rounded-md" alt={currRecipe.title} src={currRecipe.image}></img>}
+                <TabInfoBox ingredients={ingredients} directions={directions} nutritionData={nutritionData}/>
             </div>
-            {currRecipe && <img alt={currRecipe.title} src={currRecipe.image}></img>}
-            <div>
-                <h2>Directions</h2>
-                <ul className="list-disc">{directions}</ul>
-            </div>
-        </div>
+        </>
     )
 }
 export default Dish;
