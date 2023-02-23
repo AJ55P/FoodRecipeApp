aeb397e2f2f9c7951f31eed633046070e5717b38
AleJ2 <59931695+AJ55P@users.noreply.github.com>
2023-02-23 11:10:35 -0800

    Menu.js work
    * Remove the local svg imports that were commented out, since they're unneeded
    * Import the three remaining cuisine icons from the react-icons library
    * Render the icons in their corresponding positions within the menu nav

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,7 +1,8 @@
 import React from 'react';
-// import Pizza from '../images/pizza-svgrepo-com.svg';
-// import Pizza from "jsx:../images/pizza-svgrepo-com.svg";
 import { CiPizza } from 'react-icons/ci';
+import { FaPepperHot } from 'react-icons/fa';
+import { GiOlive } from 'react-icons/gi';
+import { GiHamburger } from 'react-icons/gi';
 
 function Menu({setCuisine}){
     return (
@@ -9,17 +10,17 @@ function Menu({setCuisine}){
             <ul className="flex flex-row flex-wrap justify-center gap-2">
                 <li onClick={()=> setCuisine('american')} className="bg-gradient-to-r from-orange-200 to-red-200
                 cursor-grab flex flex-col h-16 w-16 scale-90 items-center justify-center rounded-full">
-                    <CiPizza className="text-2xl"/>
+                    <GiHamburger className="text-2xl"/>
                     <a className="text-sm">American</a>
                 </li>
                 <li onClick={()=> setCuisine('mexican')} className="cursor-grab flex flex-col h-16 w-16 scale-90 items-center justify-center rounded-full 
                 bg-gradient-to-r from-orange-200 to-red-200">
-                    <CiPizza className="text-2xl"/>
+                    <FaPepperHot className="text-2xl"/>
                     <a className="text-sm">Mexican</a>
                 </li>
                 <li onClick={()=> setCuisine('greek')} className="cursor-grab flex flex-col h-16 w-16 scale-90 items-center justify-center rounded-full 
                 bg-gradient-to-r from-orange-200 to-red-200">
-                    <CiPizza className="text-2xl"/>
+                    <GiOlive className="text-2xl"/>
                     <a className="text-sm">Greek</a>
                 </li>
                 <li onClick={()=> setCuisine('italian')} className="cursor-grab flex flex-col h-16 w-16 scale-90 items-center justify-center rounded-full 
