420efff249225efcee7241deff015be38dc798dd
AleJ2 <59931695+AJ55P@users.noreply.github.com>
2023-03-02 18:56:52 -0800

    server.js work
    * Create the route handler for fetching a recipe's search results
    * Change the response to return json, in the home recipes api

--- server.mjs
+++ server.mjs
@@ -20,7 +20,7 @@ app.get('/', async function foo(req, res, next){
     }
     catch(error){
         console.log(error);
-        res.send('Could not receive the home recipes!');
+        res.json('Could not receive the home recipes!');
         //This middleware will pass on the error message to the error handler!
         //next(error);
     }
@@ -60,27 +60,28 @@ app.post('/cuisine', async (req, res) =>{
   }
   catch(error){
     console.log(error);
-    res.json("Could not receive the dish recipe");
+    res.json("Could not receive the cuisine recipes");
   }
 });
 
-
-
-// app.get('/', async (req, res) =>{
-//     try{
-//         const response_1 = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.FOOD_KEY}&diet=primal&instructionsRequired=true&addRecipeInformation=true&sort=popularity&addRecipeNutrition=true&number=6`);
-//         if(!ignore){
-//           const recipeData_1 = await response_1.json();
-//           setHomeRecipes({
-//             primal_recipes: recipeData_1.results
-//           });
-//         }
-//       }
-//       catch(error){
-//         console.log('Could not receive the home recipes!');
-//       }
-//       res.json()
-// });
+// The recipe search results api! 
+app.post('/searchrecipe', async (req, res) =>{
+  // sanitize this!
+  let recipeX = req.body.recipeName.toString();
+  try{
+      const response_1 = await fetch(`https://api.spoonacular.com/recipes/complexSearch/?apiKey=${process.env.FOOD_KEY}&query=${recipeX}&instructionsRequired=true&number=10`);
+      if(!response_1.ok){
+        throw new Error(`HTTP error: ${response_1.status} ${response_1.statusText}`);
+      }
+      const results_data = await response_1.json();
+      console.log(results_data.results);
+      res.json(results_data.results);
+  }
+  catch(error){
+    console.log(error);
+    res.json("Could not receive the dish recipe results");
+  }
+});
 
 app.listen(PORT, ()=>{
     console.log(`recipeApp/api server listening on localhost:${PORT}`);
